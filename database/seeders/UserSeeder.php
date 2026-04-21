d2029abec3f10cd97cce7f0cfaa4189248a80e9c
Agis <sarifan176@gmail.com>
2026-04-21 17:00:57 +0700

    5th sesstion end

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
-use Hash;
+use App\Models\Permission;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -14,13 +17,21 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('users')->insert([
-            'npm'       => 12345,
+        Role::create(['name' => 'mahasiswa']);
+        Role::create(['name' => 'pustakawan']);
+        Permission::create(['name' => 'show book']);
+        Permission::create(['name' => 'edit book', 'guard_name' => 'api']);
+
+        $user = User::create([
+            'npm'       => 5520123002,
             'username'  => 'Chohan123',
             'first_name'=> 'Cho',
             'last_name' => 'Han',
             'email'     => 'chohan@gmail.com',
             'password'  => Hash::make('password')
         ]);
+
+        $user->assignRole('mahasiswa');
+        $user->givePermissionTo('show book');
     }
 }
